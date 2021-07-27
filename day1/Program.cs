569121e30e225260c58df6f4ddc20467d59c75c0
Kirti-Barapatre <88020099+Kirti-Barapatre@users.noreply.github.com>
2021-07-27 14:54:18 +0530

    updated program.cs
    added cw

--- day1/Program.cs
+++ day1/Program.cs
@@ -7,6 +7,7 @@ namespace programday1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hi All");
         }
     }
 }
