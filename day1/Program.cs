57578dc896b3dddbeb126d6eb7affecc22ec1b7f
Kirti-Barapatre <kirtibarapatre05@gmail.com>
2021-07-27 14:52:26 +0530

    .net Application

---
+++ day1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace programday1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
